c44d1f80d6f6e871372f069b58e9646d9f19bd7d
nicolegut <nmg40@duke.edu>
2025-10-29 13:05:00 -0400

    Class Work Finished - line 74

--- src/HurricaneTracker.py
+++ src/HurricaneTracker.py
@@ -31,68 +31,79 @@ storm_track_points =  "memory\\track_points"
 storm_track_line = "memory\\Tracklines"
 
 #%%
+#create dictionary of storm names in seasons
+#initialize dictionary
+storm_dict = {}
+
+
 #initialize storm name list
 storm_names = []
+#iterate through years
+for storm_season in range(2000,2004):
+    #create list of storms
+    cursor = arcpy.da.SearchCursor(
+        in_table = ibtracs_NA_points, 
+        where_clause = f'SEASON = {storm_season}',
+        field_names = ['NAME']
+    )
 
-#create list of storms
-cursor = arcpy.da.SearchCursor(
-    in_table = ibtracs_NA_points, 
-    where_clause = f'SEASON = {storm_season}',
-    field_names = ['NAME']
-)
+    #with arcpy.da.SearchCursor(
+    # in_table = ibtracs_NA_points, 
+    # where_clause = f'SEASON = {storm_season}',
+    # field_names = ['NAME']
+    #) as cursor: 
+        ###code chunk and then cursor auto-deletes
 
-#with arcpy.da.SearchCursor(
-   # in_table = ibtracs_NA_points, 
-   # where_clause = f'SEASON = {storm_season}',
-   # field_names = ['NAME']
-#) as cursor: 
-    ###code chunk and then cursor auto-deletes
+    for row in cursor:
+        storm_name = row[0]
+        #append to list 
+        if not storm_name in storm_names and storm_name != 'UNNAMED':
+            storm_names.append(storm_name)
 
+    storm_dict[storm_season] = storm_names
 
-for row in cursor:
-   stormname = row[0]
-   #append to list 
-   if not stormname in storm_names and stormname != 'UNNAMED':
-    storm_names.append(stormname)
-
-#delete cursor
-del cursor
+    #delete cursor
+    del cursor
 
 # %% [markdown]
 # #### Select point features corresponding to a specific storm (season & name)
 
 # %%
+
+#iterate through keys in the dictionary
+for storm_season in storm_dict.keys:
+    #get list of storms
+    storm_names = storm_dict[storm_season]
+
+    storm_count = 0
+
+#iterate through storms
+    for storm_name in storm_names:
 #Select points for a given storm
-arcpy.analysis.Select(
-    in_features=ibtracs_NA_points, 
-    out_feature_class=storm_track_points, 
-    where_clause=f"SEASON = {storm_season} And NAME = '{storm_name}'"
-)
+        arcpy.analysis.Select(
+            in_features=ibtracs_NA_points, 
+            out_feature_class=storm_track_points, 
+            where_clause=f"SEASON = {storm_season} And NAME = '{storm_name}'"
+        )
 
-# %% [markdown]
-# #### Connect Points to a Track line
+        #Connect point to a track line
+        arcpy.management.PointsToLine(
+            Input_Features=storm_track_points,
+            Output_Feature_Class=storm_track_line,
+            Sort_Field='ISO_TIME'
+        )
 
-# %%
-#Connect point to a track line
-arcpy.management.PointsToLine(
-    Input_Features=storm_track_points,
-    Output_Feature_Class=storm_track_line,
-    Sort_Field='ISO_TIME'
-)
+        select_output = arcpy.management.SelectLayerByLocation(
+            in_layer=usa_counties, 
+            overlap_type="INTERSECT", 
+            select_features=storm_track_line, 
+            )
+        select_result = select_output.getOutput(0)
 
+        county_count = int(arcpy.GetCount_management(select_result).getOutput(0))
 
-
-# %% [markdown]
-# #### Select Counties Intersecting Point
-
-# %%
-#Select intersecting counties
-select_output = arcpy.management.SelectLayerByLocation(
-    in_layer=usa_counties, 
-    overlap_type="INTERSECT", 
-    select_features=storm_track_line, 
-    )
-select_result = select_output.getOutput(0)
+        storm_count += county_count
+        print(storm_name, storm_season, county_count, storm_count)
 
 
 # %% [markdown]
@@ -101,7 +112,7 @@ select_result = select_output.getOutput(0)
 #%%
 #count the counties
 
-county_count = int(arcpy.GetCount_management(select_result).getOutput(0))
+
 
 
 # %%
